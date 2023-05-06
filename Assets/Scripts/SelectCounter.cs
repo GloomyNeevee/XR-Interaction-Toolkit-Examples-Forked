9861e184e6cdddfb32bcb9a826c128767fca2ca5
GloomyNeevee <78270544+GloomyNeevee@users.noreply.github.com>
2023-05-06 15:32:33 +0200

    Added SelectCounter Script

---
+++ Assets/Scripts/SelectCounter.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectCounter : MonoBehaviour
+{
+    [SerializeField] public int hoverCounter { get; private set; }
+    public void OnHoverCounter()
+    {
+        Debug.Log("Entered Cube");
+        hoverCounter++;
+        Debug.Log(hoverCounter);
+    }
+}
